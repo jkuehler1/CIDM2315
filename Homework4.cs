fd34470c1a6c53e8e4094e9a1143af0dea87fcdc
jkuehler1 <112365315+jkuehler1@users.noreply.github.com>
2022-09-21 22:00:22 -0500

    Add files via upload

---
+++ Homework4.cs
@@ -0,0 +1,35 @@
+﻿namespace Homework3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Q1
+    {
+            Console.Write("a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+            int largest_number = largest(a, b);
+            Console.WriteLine("The largest number is: " + largest_number);
+        }
+        static int largest(int a, int b) {
+            if (a >= b)
+                return a;
+            else
+                return b;
+        }
+    }
+    }
+// I don't know how to do Q2
+
+    
+    
+
+    
+   
+    
+
+
+
